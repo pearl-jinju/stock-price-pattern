432b57f308b01b744ffeb283280564d88beb72cb
JInJu Jeong <y2kwlswn@Naver.com>
2024-02-17 12:14:37 +0900

    검색효율 증대를 위한 종목명(티커)형태로 변환

--- stockchaser/stockpattern/views.py
+++ stockchaser/stockpattern/views.py
@@ -17,12 +17,14 @@ class MainPage(APIView):
         df_name_ticker = pd.DataFrame(df_name_ticker.values_list()).iloc[:,1:]
         df_name_ticker.columns = ['종목명','티커']
         df_name_ticker = df_name_ticker.sort_values(by="종목명")
-        df_name = df_name_ticker.iloc[:,:1].values.tolist()
-        df_name = [name[0] for name in df_name]
-        df_name =",".join(df_name)
-        df_ticker = df_name_ticker.iloc[:,1:2].values.tolist()
-        df_ticker = [ticker[0] for ticker in df_ticker]
-        df_ticker =",".join(df_ticker)
+        name_list = df_name_ticker.iloc[:,:1].values.tolist()
+        name_list = [name[0] for name in name_list]
+
+        ticker_list = df_name_ticker.iloc[:,1:2].values.tolist()
+        ticker_list = [ticker[0] for ticker in ticker_list]
+
+        name_ticker_list = [name+" ("+ticker+")" for name, ticker in zip(name_list,ticker_list)]
+        name_ticker_list =",".join(name_ticker_list)
         # print(df_name , df_ticker)
         
         # df =  pd.DataFrame(StockPriceDateBase.objects.filter(name="더존비즈온").values_list())
@@ -31,4 +33,4 @@ class MainPage(APIView):
         # df = df.sort_values(by="date",ascending=True)
         # print(df)
         # return render(request,"main/main.html",context=dict(datafeed=df)) #context html로 넘길것
-        return render(request,"main/main.html", {'df_name': df_name,'df_ticker':df_ticker},status=200) #context html로 넘길것
+        return render(request,"main/main.html", {'name_ticker_list': name_ticker_list,},status=200) #context html로 넘길것
